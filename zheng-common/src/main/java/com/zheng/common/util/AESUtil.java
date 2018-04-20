caea0d1cb276ff06d1b92ae939b75c330cde02a2
amykiki <staramy_2005@126.com>
2018-04-20 23:16:07 +0800

    修改数据库密码

--- zheng-common/src/main/java/com/zheng/common/util/AESUtil.java
+++ zheng-common/src/main/java/com/zheng/common/util/AESUtil.java
@@ -129,7 +129,7 @@ public class AESUtil {
 
     public static void main(String[] args) {
         String[] keys = {
-                "java0502", "123456"
+                "java0502", "123@Aa456"
         };
         System.out.println("key | AESEncode | AESDecode");
         for (String key : keys) {
